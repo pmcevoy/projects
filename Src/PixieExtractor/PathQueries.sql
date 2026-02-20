65ea9bb604ea05b173973897d5c836b3c6584f85
Peter McEvoy <peter.m.mcevoy@gmail.com>
2026-02-20 16:36:43 +0000

    Generate file moves

--- Src/PixieExtractor/PathQueries.sql
+++ Src/PixieExtractor/PathQueries.sql
@@ -1,59 +1,43 @@
-SELECT TOP 100 
-	'/' +
-	RIGHT( REPLICATE('0', 3) + CAST(FileSystemID as VARCHAR(3)), 3)
-	+ '/' +
-	RIGHT( REPLICATE('0', 3) + CAST(SubSystemID as VARCHAR(3)), 3)
-	+ '/' +
-	SUBSTRING(CAST(FileUID as CHAR(36)), 1, 2)
-	+ '/' +
-	SUBSTRING(CAST(FileUID AS CHAR(36)), 3, 2)
-	+ '/' +
-	CAST(FileUID AS CHAR(36)) 
-+ '.jpg' AS SourcePath,
-	'/' +
-	Username
-	+ '/' + 
-	UploadBatchID 
-	+ '/' + 
-	ItemName
-	+ '.jpg'  AS TargetPath
-FROM Ark_Item 
-INNER JOIN Ark_User
-	ON Ark_Item.OwnerAccountID = Ark_User.ActiveAccountID
-WHERE PathVersion = 1
-
-
-SELECT TOP 100 
-	'/' +
-	RIGHT( REPLICATE('0', 3) + CAST(FileSystemID as VARCHAR(3)), 3)
-	+ '/' +
-	RIGHT( REPLICATE('0', 3) + CAST(SubSystemID as VARCHAR(3)), 3)
-	+ '/' +
-	SUBSTRING(CAST(FileUID as CHAR(36)), 1, 2)
-	+ '/' +
-	SUBSTRING(CAST(FileUID AS CHAR(36)), 3, 2)
-	+ '/' +
-	CAST(FileUID AS CHAR(36)) 
-+ '.jpg' AS SourcePath,
-	'/' +
-	Username
-	+ '/' + 
-	UploadBatchID 
-	+ '/' + 
-	ItemName
-	+ '.jpg'  AS TargetPath
-FROM Ark_Item 
-INNER JOIN Ark_User
-	ON Ark_Item.OwnerAccountID = Ark_User.ActiveAccountID
-WHERE PathVersion = 2
-
-
---SELECT DISTINCT Rotation
---FROM Ark_Item
---GROUP BY ItemName
-----HAVING COUNT(*) > 1
-
---SELECT Username, COUNT(*)
---FROM Ark_User
---GROUP BY UserName
---HAVING COUNT(*) > 1
+	SELECT
+			RIGHT( REPLICATE('0', 3) + CAST(SubSystemID as VARCHAR(3)), 3) + '\' +
+			SUBSTRING(CAST(FileUID as CHAR(36)), 1, 2) + '\' +
+			SUBSTRING(CAST(FileUID AS CHAR(36)), 3, 2) + '\' +
+			REPLACE( CAST(FileUID AS CHAR(36)), '-', '') + '.jpg' 
+		AS SourcePath,
+			'\' + Username + '\' + convert(varchar, ItemDate, 23) + '\' + ItemName + '.jpg' 
+		AS TargetPath
+	FROM Ark_Item 
+	INNER JOIN Ark_User
+		ON Ark_Item.OwnerAccountID = Ark_User.ActiveAccountID
+	WHERE PathVersion = 1
+UNION
+	SELECT
+			RIGHT( REPLICATE('0', 3) + CAST(SubSystemID as VARCHAR(3)), 3) + '\' +
+			SUBSTRING(CAST(FileUID as CHAR(36)), 1, 2) + '\' +
+			SUBSTRING(CAST(FileUID AS CHAR(36)), 3, 2) + '\' +
+			REPLACE( CAST(FileUID AS CHAR(36)), '-', '') + '-' +
+			RIGHT( REPLICATE('0', 10) + CAST(OwnerAccountID AS VARCHAR(10)), 10) + '-' +
+			RIGHT( REPLICATE('0', 10) + CAST(ItemID AS VARCHAR(10)), 10) + '-' +
+			RIGHT( REPLICATE('0', 5) + CAST(GREATEST(Width,Height) AS VARCHAR(5)), 5) + 'L'	+ '-' +
+			REPLACE( CAST(PathSecret AS CHAR(36)), '-', '') +
+			CASE MediaType 
+				WHEN 'image/jpeg' THEN '.jpg'
+				WHEN 'image/png' THEN '.png'
+				WHEN 'image/gif' THEN '.gif'
+				WHEN 'image/bmp' THEN '.bmp'
+				WHEN 'image/tiff' THEN '.tif'
+			END 
+		AS SourcePath,
+			'\' + Username + '\' + convert(varchar, ItemDate, 23) + '\' + ItemName + 
+			CASE MediaType 
+				WHEN 'image/jpeg' THEN '.jpg'
+				WHEN 'image/png' THEN '.png'
+				WHEN 'image/gif' THEN '.gif'
+				WHEN 'image/bmp' THEN '.bmp'
+				WHEN 'image/tiff' THEN '.tif'
+			END 
+		AS TargetPath
+	FROM Ark_Item
+	INNER JOIN Ark_User
+		ON Ark_Item.OwnerAccountID = Ark_User.ActiveAccountID
+	WHERE PathVersion = 2
