e75e6ae1f2afcde5ecf2eba721ad4dd3ade726d8
Peter McEvoy <peter.m.mcevoy@gmail.com>
2026-02-20 16:37:19 +0000

    Script to evaluate if we have source naming correct

---
+++ Src/PixieExtractor/TestFiles.ps1
@@ -0,0 +1,9 @@
+Import-Csv -Path .\PixieFiles.csv |  %{
+	$sourcePath = "\\100.72.72.88\pixie\$($_.SourcePath)"
+	if( Test-Path $sourcePath ){
+		Write-Host "Found $sourcePath"
+	}
+	else {
+		Write-Host "Could not find $sourcePath" -ForegroundColor Red
+	}
+}
