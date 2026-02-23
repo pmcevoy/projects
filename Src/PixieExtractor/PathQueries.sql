2bc144269dbb6af70d8295e5034fcde8382d74ce
Peter McEvoy <peter.m.mcevoy@gmail.com>
2026-02-23 17:53:44 +0000

    Change generated file based on runtime feedback

--- Src/PixieExtractor/PathQueries.sql
+++ Src/PixieExtractor/PathQueries.sql
@@ -1,17 +1,21 @@
-	SELECT TOP 50
+	SELECT --TOP 50
 			RIGHT( REPLICATE('0', 3) + CAST(SubSystemID as VARCHAR(3)), 3) + '\' +
 			SUBSTRING(CAST(FileUID as CHAR(36)), 1, 2) + '\' +
 			SUBSTRING(CAST(FileUID AS CHAR(36)), 3, 2) + '\' +
 			REPLACE( CAST(FileUID AS CHAR(36)), '-', '') + '.jpg' 
 		AS SourcePath,
-			Username + '\' + convert(varchar, ItemDate, 23) + '\' + ItemName + + '-' + CONVERT(VARCHAR, ItemID, 8) + '.jpg' 
-		AS TargetPath
+		Username,
+		CONVERT(VARCHAR, ItemDate, 23) AS ItemDate,
+		ItemID,
+		'.jpg' AS Extension,
+		ItemName
 	FROM Ark_Item 
 	INNER JOIN Ark_User
 		ON Ark_Item.OwnerAccountID = Ark_User.ActiveAccountID
 	WHERE PathVersion = 1
+	AND OwnerAccountID <> 1
 UNION
-	SELECT TOP 50
+	SELECT --TOP 50
 			RIGHT( REPLICATE('0', 3) + CAST(SubSystemID as VARCHAR(3)), 3) + '\' +
 			SUBSTRING(CAST(FileUID as CHAR(36)), 1, 2) + '\' +
 			SUBSTRING(CAST(FileUID AS CHAR(36)), 3, 2) + '\' +
@@ -28,16 +32,21 @@ UNION
 				WHEN 'image/tiff' THEN '.tif'
 			END 
 		AS SourcePath,
-			Username + '\' + convert(varchar, ItemDate, 23) + '\' + ItemName + + '-' + CONVERT(VARCHAR, ItemID, 8) +
-			CASE MediaType 
-				WHEN 'image/jpeg' THEN '.jpg'
-				WHEN 'image/png' THEN '.png'
-				WHEN 'image/gif' THEN '.gif'
-				WHEN 'image/bmp' THEN '.bmp'
-				WHEN 'image/tiff' THEN '.tif'
-			END 
-		AS TargetPath
+		Username,
+		CONVERT(VARCHAR, ItemDate, 23) AS ItemDate,
+		ItemID,
+		CASE MediaType 
+			WHEN 'image/jpeg' THEN '.jpg'
+			WHEN 'image/png' THEN '.png'
+			WHEN 'image/gif' THEN '.gif'
+			WHEN 'image/bmp' THEN '.bmp'
+			WHEN 'image/tiff' THEN '.tif'
+		END 
+		AS Extension,
+		ItemName
 	FROM Ark_Item
 	INNER JOIN Ark_User
 		ON Ark_Item.OwnerAccountID = Ark_User.ActiveAccountID
 	WHERE PathVersion = 2
+	AND OwnerAccountID <> 1
+ORDER BY SourcePath
