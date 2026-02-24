20f57ab90f2e5b2d6ad5f3c82b02d42a01a60cd5
Peter McEvoy <peter.m.mcevoy@gmail.com>
2026-02-24 13:49:13 +0000

    Cleanup the itenmame for OS path

--- Src/PixieExtractor/CopyFilesToUserDir.ps1
+++ Src/PixieExtractor/CopyFilesToUserDir.ps1
@@ -5,33 +5,34 @@ $csvFilePath = ".\PixieFiles-Small.csv"
 $totalRecords = (Get-Content $csvFilePath | Measure-Object).Count - 1
 $recordCounter = 0
 
-Get-Content -Path $csvFilePath |  %{
-	$parts = $_.Split(",")
-	($sourcePath,$username,$itemDate,$itemID,$extension,$itemNameNotJoined) = $parts
-	$itemName = $itemNameNotJoined -join ","  #Re-add any commas that were removed by split
+Get-Content -Path $csvFilePath |
+	Select-Object -Skip 1 |
+	%{
+		$parts = $_.Split(",")
+		($sourcePath,$username,$itemDate,$itemID,$extension,$itemNameNotJoined) = $parts
+		$itemName = $itemNameNotJoined -join ","  #Re-add any commas that were removed by split
+		$safeItemName = $itemName -replace '[\\/:*?"<>|]', '' #strip out invalid chars
 
-	$src = "$($sourceRoot)\$($sourcePath)"
-	$dest = "$($destRoot)\$($_.TargetPath)"
-	$percentComplete = ($recordCounter++ / $totalRecords) * 100
-	Write-Progress -Activity "Copying" -Status "$($percentComplete)% complete" -PercentComplete $percentComplete
+		$src = "$($sourceRoot)\$($sourcePath)"
+		$dest = "$($destRoot)\$($username)\$($itemDate)\$($safeItemName)-$($itemID)$($extension)"
+		$percentComplete = ($recordCounter++ / $totalRecords) * 100
+		Write-Progress -Activity "Copying" -Status "$($percentComplete)% complete" -PercentComplete $percentComplete
 
-	if( !(Test-Path $src) ){
-		Write-Host "Could not find $src" -ForegroundColor Red
-		return
+		if( !(Test-Path $src) ){
+			Write-Host "Could not find $src" -ForegroundColor Red
+			return
+		}
+		if( (Test-Path $dest) ){
+			Write-Host "Skipping as it allready exists: $dest" -ForegroundColor DarkGray
+			return
+		}
+
+		if( !(Test-Path "$($destRoot)\$($username)") ){
+			New-Item -Type Directory "$($destRoot)\$($username)\"  | Out-Null
+		}
+		if( !(Test-Path "$($destRoot)\$($username)\$($itemDate)") ){
+			New-Item -Type Directory "$($destRoot)\$($username)\$($itemDate)\" | Out-Null
+		}
+
+		Copy-Item -Path $src -Destination $dest
 	}
-	if( (Test-Path $dest) ){
-		Write-Host "Skipping as it allready exists: $dest" -ForegroundColor DarkGray
-		return
-	}
-
-
-	if( !(Test-Path "$($destRoot)\$($username)") ){
-		New-Item -Type Directory "$($destRoot)\$($username)\"  | Out-Null
-	}
-	if( !(Test-Path "$($destRoot)\$($username)\$($date)") ){
-		New-Item -Type Directory "$($destRoot)\$($username)\$($date)\" | Out-Null
-	}
-
-	Copy-Item -Path $src -Destination $dest 
-}
-
