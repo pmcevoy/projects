6f15a737506dd402049ddccb80a26207f09bde1b
Peter McEvoy <peter.m.mcevoy@gmail.com>
2026-05-06 10:23:03 +0100

    Custom prompt with subshell creds

--- Src/DotFiles/PowerShell/profile.ps1
+++ Src/DotFiles/PowerShell/profile.ps1
@@ -1,4 +1,19 @@
+function prompt {
+    $origLastExitCode = $LASTEXITCODE
+
+	$prompt += Get-AwsPrompt
+
+    $prompt += Write-Prompt "$($ExecutionContext.SessionState.Path.CurrentLocation)"
+    $prompt += Write-VcsStatus
+    $prompt += Write-Prompt "$(if ($PsDebugContext) {' [DBG]: '} else {''})" -ForegroundColor Magenta
+    $prompt += "$('>' * ($nestedPromptLevel + 1)) "
+
+    $LASTEXITCODE = $origLastExitCode
+    $prompt
+}
+
 Import-Module posh-git
+Import-Module TerraformHelpers
 
 #Disable directory higlighting for dir
 #https://github.com/PowerShell/PowerShell/issues/18778#issuecomment-1354060311
@@ -7,3 +22,5 @@ $PSStyle.FileInfo.Directory = "`e[34m"
 # Enable kubectl autocomplete
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-windows/#enable-shell-autocompletion
 kubectl completion powershell | Out-String | Invoke-Expression
+
+
